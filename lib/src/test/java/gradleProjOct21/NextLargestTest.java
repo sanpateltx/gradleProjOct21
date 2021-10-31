66ec19690983afea9a9a57c83e91ad83ea2be462
sandip patel <44559492+sanpateltx@users.noreply.github.com>
2021-10-30 21:29:32 -0500

    added a negative test case

--- lib/src/test/java/gradleProjOct21/NextLargestTest.java
+++ lib/src/test/java/gradleProjOct21/NextLargestTest.java
@@ -1,8 +1,8 @@
 package gradleProjOct21;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
 
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 //import com.home.NextLargest;
 
@@ -17,11 +17,13 @@ public class NextLargestTest {
 		char[] digits = { '5','3','4','9','7','6' };
 		  			
 		assertEquals("536479",nextLargest.findLargest(digits, digits.length));
-		
-		
-		 digits = null;
+
+		assertNotEquals("534976",nextLargest.findLargest(digits, digits.length));
+
+		digits = null;
 
 		assertNull(nextLargest.findLargest(digits, 0));
+
 	}
 
 }
